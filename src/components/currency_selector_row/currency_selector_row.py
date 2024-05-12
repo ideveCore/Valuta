462a2776173e6d5e119e3e21c1851a510391278b
Ideve Core <ideve.core@gmail.com>
2024-05-11 22:04:29 -0300

    Change application styles

--- src/components/currency_selector_row/currency_selector_row.py
+++ src/components/currency_selector_row/currency_selector_row.py
@@ -30,7 +30,7 @@ class CurrencySelectorRow(Gtk.ListBoxRow):
     def __init__(self, currency):
         super().__init__()
         self.currency = currency
-        self.name.props.label = f'{CODES[str(self.currency)]["flag"]}   {self.currency} - {self.currency.name}'
+        self.name.props.label = f'{CODES[str(self.currency)]["flag"]}   {self.currency} – {self.currency.name}'
 
         self.currency.bind_property(
             'selected',
